95257dfe97c5d150d0442808640beb2bf33b0915
Symthy <sympathia3104@gmail.com>
2022-10-10 11:27:13 +0900

    ci: build and test

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext' //ref: https://github.com/vitejs/vite/issues/6985#issuecomment-1044375490
+  },
   plugins: [react({ jsxImportSource: '@emotion/react' }), tsconfigPaths()]
 });
