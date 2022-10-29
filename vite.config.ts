03640b5d189e5a55f618e81903b4d233e3d0f2f0
Symthy <sympathia3104@gmail.com>
2022-10-30 02:11:51 +0900

    ci: miss github page url

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
     target: 'esnext' // ref: https://github.com/vitejs/vite/issues/6985#issuecomment-1044375490
   },
   plugins: [react({ jsxImportSource: '@emotion/react' }), tsconfigPaths()],
-  base: process.env.GITHUB_PAGES ? 'Symthy' : './' // Github Pages 用　ref: https://zenn.dev/shivase/articles/009-react-vite-githubpages
+  base: process.env.GITHUB_PAGES ? 'react-clone-yumemi-exam' : './' // Github Pages 用　ref: https://zenn.dev/shivase/articles/009-react-vite-githubpages
 });
