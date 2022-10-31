bbf157fe7a54d93f22306ed5844f85c66b9ac1c7
Symthy <sympathia3104@gmail.com>
2022-11-01 00:18:01 +0900

    ci: preventing 404 display on github pages (third. second failure)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: 'esnext' // ref: https://github.com/vitejs/vite/issues/6985#issuecomment-1044375490
+    target: 'esnext', // ref: https://github.com/vitejs/vite/issues/6985#issuecomment-1044375490
+    rollupOptions: {
+      input: {
+        main: `${__dirname}/index.html`,
+        notfound: `${__dirname}/404.html`
+      }
+    }
   },
   plugins: [react({ jsxImportSource: '@emotion/react' }), tsconfigPaths()],
   base: process.env.GITHUB_PAGES ? 'react-clone-yumemi-exam' : './' // Github Pages 用　ref: https://zenn.dev/shivase/articles/009-react-vite-githubpages
