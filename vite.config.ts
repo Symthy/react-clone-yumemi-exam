5df4da90af1aeac85783c48f05a129ef96ff6d31
Symthy <sympathia3104@gmail.com>
2022-11-02 00:58:42 +0900

    ci: include redirect script when build for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,38 @@
+import * as fs from 'fs';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+// ref: https://dev.classmethod.jp/articles/vite-index-html-read-env-variables/
+// ref: https://vitejs.dev/guide/api-plugin.html#transformindexhtml
+const htmlPlugin = () => {
+  return {
+    name: 'html-transform',
+    enforce: 'pre' as const,
+    transformIndexHtml(html: string) {
+      return html.replace(
+        /    %SCRIPT_FOR_GITHUB_PAGES%/g,
+        process.env.GITHUB_PAGES ? fs.readFileSync('./ghpages/script.txt', 'utf8') : ''
+      );
+    }
+  };
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: 'esnext', // ref: https://github.com/vitejs/vite/issues/6985#issuecomment-1044375490
     rollupOptions: {
-      input: {
-        main: `${__dirname}/index.html`,
-        notfound: `${__dirname}/404.html`
-      }
+      input: process.env.GITHUB_PAGES
+        ? {
+            index: `${__dirname}/index.html`,
+            notfound: `${__dirname}/404.html`
+          }
+        : {
+            index: `${__dirname}/index.html`
+          }
     }
   },
-  plugins: [react({ jsxImportSource: '@emotion/react' }), tsconfigPaths()],
+  plugins: [react({ jsxImportSource: '@emotion/react' }), tsconfigPaths(), htmlPlugin()],
   base: process.env.GITHUB_PAGES ? 'react-clone-yumemi-exam' : './' // Github Pages 用　ref: https://zenn.dev/shivase/articles/009-react-vite-githubpages
 });
