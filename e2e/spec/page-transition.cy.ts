1185fdd69bbb290385795d498ae35d09c9d935f2
Symthy <sympathia3104@gmail.com>
2022-11-19 15:48:36 +0900

    ci: fix url

--- e2e/spec/page-transition.cy.ts
+++ e2e/spec/page-transition.cy.ts
@@ -1,6 +1,6 @@
 import { SetupWorkerApi, rest, ResponseResolver, MockedRequest, restContext } from 'msw';
 
-const APP_URL = 'http://127.0.0.1:5173';
+const APP_URL = 'http://localhost:5173'; //'http://127.0.0.1:5173';
 type MswHolder = {
   msw: { worker: SetupWorkerApi; rest: typeof rest };
 };
